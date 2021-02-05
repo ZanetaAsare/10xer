3edcce7bdcd787ed95418d1234573cca3c8331bb
nunyaklah <nunyaklah@gmail.com>
2021-02-05 12:11:45 +0000

    commented

---
+++ JavaProject1/src/Lecture.java
@@ -0,0 +1,24 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+//public class Lecture {
+//    private List<Student> students = new ArrayList<Student>();
+//
+//    public void enter(Student std){
+//        this.students.add(std);
+//    }
+//
+//    public double getHighestAverageGrade(){
+//        List<Double> studentAverageGrades = new ArrayList<Double>();
+//
+//        for (Student std :this.students){
+//            studentAverageGrades.add(std.getAverageGrade());
+//        }
+//
+//        double maxVal = Collections.max(studentAverageGrades);
+//
+//        return  maxVal;
+//    }
+//
+//
---
+++ JavaProject1/test/LectureTest.java
@@ -0,0 +1,15 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class LectureTest {
+//  void getHighestAverageGrade(){
+//      List<Double> grades = new ArrayList<Double>();
+//      grades.add(15.0);
+//      grades.add(15.0);
+//      grades.add(15.0);
+//
+//
+//  }
+//}
