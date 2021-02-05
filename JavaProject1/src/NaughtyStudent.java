d815882161c4c44a5fa3eb3fe5b20e5bf8df6a4a
nunyaklah <nunyaklah@gmail.com>
2021-02-05 15:00:59 +0000

    Naughty student checks

--- JavaProject1/src/NaughtyStudent.java
+++ JavaProject1/src/NaughtyStudent.java
@@ -1,3 +1,14 @@
-public class NaughtyStudent {
+import java.util.List;
 
+public class NaughtyStudent extends Student{
+
+
+    public NaughtyStudent(List<Double> grades) {
+        super(grades);
+    }
+
+    public double getNaughtyAverageGrade(){
+        double naughtyGrade = (super.getAverageGrade() * 0.1) + super.getAverageGrade() ;
+        return naughtyGrade;
+    }
 }
